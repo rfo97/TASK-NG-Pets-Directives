6a3b389564a84d372f8935fb065aa18ea147f96c
rfo97 <rfa.alobaid@gmail.com>
2025-04-07 09:53:28 +0300

    fix: add ngIF and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-
 import pets from '../data/pets';
 import { PetComponent } from './pet/pet.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [PetComponent],
+  imports: [PetComponent, NgFor, NgIf],
 })
 export class AppComponent {
   title = 'TASK-NG-Pets-Directives';
